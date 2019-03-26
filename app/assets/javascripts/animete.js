6ac57af1b08f613ea4ab000599ca51302e07a5c8
luglio22 <sweet.c.july22@gmail.com>
2019-03-26 17:11:51 +0900

    投稿アニメーション

--- app/assets/javascripts/animete.js
+++ app/assets/javascripts/animete.js
@@ -5,6 +5,16 @@ $(document).on("turbolinks:load",function(){
     e.preventDefault();
     return false;
   })
+  //投稿 アニメーション
+  //投稿　表示
+  $(".vote-link").click(function(){
+    $(".product-vote").css("display","block")
+  })
+  $(".vote-link").click(function(e){
+    $('html, body').animate({scrollTop:$('.product-vote').offset().top});
+    e.preventDefault();
+    return false;
+  })
   //about this
   $(".about-link").click(function(e){
     $('html, body').animate({scrollTop:$('.about-this').offset().top});

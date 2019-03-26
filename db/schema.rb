d14e3cd2a1210f02f5fcdd10cf3932d0fbd35e55
luglio22 <sweet.c.july22@gmail.com>
2019-03-26 14:57:33 +0900

    Model product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_20_061840) do
+ActiveRecord::Schema.define(version: 2019_03_26_055654) do
+
+  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false

8be97ff4509f5444cb23a73efa3c45f480af5bad
luglio22 <sweet.c.july22@gmail.com>
2019-08-14 23:27:07 +0900

    removeLogoutAlert

--- app/assets/javascripts/alert.js
+++ app/assets/javascripts/alert.js
@@ -2,7 +2,6 @@ $(document).on("turbolinks:load",function(){
   //新規登録
   $(".logout-link").click(function(e){
     var result = window.confirm( 'ログアウトしてもよろしいですか？');
-    alert(result);
     if(result == false){
       return false;
       e.preventDefault();
